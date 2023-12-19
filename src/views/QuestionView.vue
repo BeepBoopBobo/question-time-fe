0ed818a78b4f1ee982d93d55ff1ef7be3454c39b
BeepBoopBobo <marek.tomas.mk@gmail.com>
2023-12-19 18:40:25 +0100

    add: question page

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -44,9 +44,100 @@ export default {
 </script>
 
 <template>
-    <h3>Question: {{ question.name }}</h3>
-    <div id="answers">
-        <button class="answer-btn" v-for="answer in question.answers" :key="answer.id">{{ answer }}</button>
+    <h3 class="id-card">{{ question.id }} </h3>
+    <span id="question">{{ question.name }}</span>
+    <div id="image-container">
+        <img id="image" src="">
+        <div id="image-tape"></div>
+    </div>
+    <div id="answers-container">
+        <button class="btn answer-btn" v-for="answer in question.answers" :key="answer.id">
+            <span class="answer-text">
+                {{ answer }}
+            </span>
+        </button>
     </div>
 </template>
-<style></style>
+<style>
+#question {
+    width: 60%;
+    margin: 0 auto 0 auto;
+    height: fit-content;
+}
+
+#image-tape {
+    margin: auto;
+    margin-bottom: -1.2rem;
+    background-color: rgba(221, 178, 112, 0.5);
+    width: 50%;
+    height: 3rem;
+    z-index: 5;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+
+img {
+    z-index: 1;
+    width: 40%;
+    margin-top: 1.2rem;
+    aspect-ratio: 1/1;
+    background-color: var(--blue);
+    border: 1rem solid var(--white);
+    box-sizing: border-box;
+}
+
+.id-card {
+    width: fit-content;
+    margin: auto;
+    margin-top: 2rem;
+    padding: 0.5rem 1rem;
+    border-radius: 8px;
+    text-transform: uppercase;
+    color: var(--text);
+}
+
+#image-container {
+    position: relative;
+    width: 60%;
+    margin: auto;
+}
+
+#answers-container {
+    width: 75%;
+    display: flex;
+    flex-wrap: wrap;
+    margin: auto;
+    gap: 10%;
+}
+
+.answer-btn {
+    width: 45%;
+    padding: 1rem;
+    margin-top: 0.5rem;
+    color: var(--white);
+    box-shadow: 10px 2px 6px 0px rgba(0, 0, 0, 0.25);
+}
+
+.answer-text {
+    text-transform: uppercase;
+    text-shadow: 1px 1px 5px var(--text);
+}
+
+.answer-btn:nth-of-type(1) {
+    background-color: var(--pink);
+}
+
+.answer-btn:nth-of-type(2) {
+    background-color: var(--green);
+}
+
+.answer-btn:nth-of-type(3) {
+    background-color: var(--blue);
+}
+
+.answer-btn:nth-of-type(4) {
+    background-color: var(--yellow);
+}
+</style>
